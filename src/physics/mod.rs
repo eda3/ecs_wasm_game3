0f19f1c0bc8834ff5142dfeb34276acb42eb7051
eda3 <gerorin1010@gmail.com>
2025-04-13 10:28:49 +0900

    エラー24

--- src/physics/mod.rs
+++ src/physics/mod.rs
@@ -339,6 +339,17 @@ impl PhysicsWorld {
     }
 }
 
+// Resourceトレイトの実装
+impl Resource for PhysicsWorld {
+    fn as_any(&self) -> &dyn std::any::Any {
+        self
+    }
+    
+    fn as_any_mut(&mut self) -> &mut dyn std::any::Any {
+        self
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
